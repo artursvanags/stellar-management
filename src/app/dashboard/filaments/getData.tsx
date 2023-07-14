1fad84b32ff358e2d071a865f08b64df4ca06976
Arturs <arturs.vanags320@gmail.com>
2023-07-14 13:53:58 +0300

    Updated useSWR into custom hook.

--- src/app/dashboard/filaments/getData.tsx
+++ src/app/dashboard/filaments/getData.tsx
@@ -1,14 +1,24 @@
-'use client'
 import { columns } from "./columns";
 import { DataTable } from "@/components/dashboard/dataTable";
 import useSWR from 'swr'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
-export default function getData() {
+
+export function getData() {
     const { data, error, isLoading } = useSWR('http://localhost:3000/api/filaments', fetcher);
     
+    return {
+        data,
+        isLoading,
+        error
+    }
+}
+
+export default function setData() {
+    const { data, error, isLoading } = getData();
+    
     if (error) return <div>Failed to load</div>;
-    if (!data) return <div>Loading...</div>;
+    if (isLoading) return <div>Loading...</div>;
 
     return (
         <>
