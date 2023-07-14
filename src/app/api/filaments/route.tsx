99684bfd5a10b41460a05aaf2b36eaf8a33c1b5d
Arturs <arturs.vanags320@gmail.com>
2023-07-14 17:46:54 +0300

    Test changes.

--- src/app/api/filaments/route.tsx
+++ src/app/api/filaments/route.tsx
@@ -7,5 +7,10 @@ import { Database } from '@/types/db'
 export async function GET(request: Request): Promise<NextResponse> {
   const supabase = createRouteHandlerClient<Database>({cookies});
   const { data: filaments } = await supabase.from('filaments').select('*')
-  return NextResponse.json(filaments, { status: 200 })
+  return NextResponse.json(filaments, { status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    } })
 }
