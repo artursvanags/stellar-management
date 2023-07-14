f555c536dd1336c7a1618ed10cd25d296ebbdb23
Arturs <arturs.vanags320@gmail.com>
2023-07-14 17:27:51 +0300

    Added Dynamic URL handler.

--- src/components/auth/user-auth-form.tsx
+++ src/components/auth/user-auth-form.tsx
@@ -34,8 +34,20 @@ interface SocialAuthButtonProps {
 
 type FormData = z.infer<typeof userAuthSchema>;
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+  return url
+}
+
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-  const redirect_path = `/auth/callback/`;
+  const redirect_path = `${getURL()}/auth/callback/`;
   const [isLoading, setIsLoading] = useState(false);
   
   const supabase = createClientComponentClient<Database>();
