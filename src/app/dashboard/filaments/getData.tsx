c8ce7e93929a442ee295cdd2ed55c4ebd767ab66
Arturs <arturs.vanags320@gmail.com>
2023-07-14 16:01:54 +0300

    Changed useSWR to SWR.

--- src/app/dashboard/filaments/getData.tsx
+++ src/app/dashboard/filaments/getData.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { columns } from "./columns";
 import { DataTable } from "@/components/dashboard/dataTable";
-import useSWR from 'swr'
-import axios from 'axios'
+import SWR from 'swr';
+import axios from 'axios';
 
 const fetcher = ( url: string ) => axios.get(url).then(res => res.data)
 
 export default function getData() {
-    const { data, error, isLoading } = useSWR('http://localhost:3000/api/filaments', fetcher);
+    const { data, error, isLoading } = SWR('http://localhost:3000/api/filaments', fetcher);
     
     if (error) return <div>Failed to load</div>;
     if (!data) return <div>Loading...</div>;
