a644ab2344538a75a945264836e2ce7af84c4875
Arturs <arturs.vanags320@gmail.com>
2023-07-19 12:04:52 +0300

    Imported getURL.

--- src/components/auth/user-auth-form.tsx
+++ src/components/auth/user-auth-form.tsx
@@ -21,7 +21,7 @@ import { Input } from '@/components/ui/input';
 import { Loader2 } from "lucide-react";
 import { Label } from '@/components/ui/label';
 import { toast } from '@/components/ui/use-toast';
-import { cn } from '@/lib/utils';
+import { cn, getURL } from '@/lib/utils';
 
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
@@ -34,17 +34,6 @@ interface SocialAuthButtonProps {
 
 type FormData = z.infer<typeof userAuthSchema>;
 
-const getURL = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:3000/'
-  // Make sure to include `https://` when not localhost.
-  url = url.includes('http') ? url : `https://${url}`
-  // Make sure to include a trailing `/`.
-  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
-  return url
-}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const redirect_path = `${getURL()}/auth/callback/`;
@@ -57,6 +46,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const spinnerClass = <Loader2 className="mr-2 h-4 w-4 animate-spin" />
   const socialAuthProviders = [
     { providerName: 'Github', authType: 'github' },
+    { providerName: 'Google', authType: 'google' },
   ]; 
 
   const {
