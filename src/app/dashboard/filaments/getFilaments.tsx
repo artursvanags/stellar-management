59ff5b62d50a060863f24e8ae069557741313b88
Arturs <arturs.vanags320@gmail.com>
2023-07-20 21:34:50 +0300

    Deleted files.

--- src/app/dashboard/filaments/getFilaments.tsx
+++
@@ -1,8 +0,0 @@
-import SWR from 'swr';
-
-const fetcher = ( url: string ) => fetch(url).then(res => res.json());
-
-export default function getFilaments() {
-    const { data, error } = SWR(`/api/filaments`, fetcher);
-    return { data, error };
-}
