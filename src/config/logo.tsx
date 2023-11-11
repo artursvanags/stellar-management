30896ac38109b3091141fb844eb6290f4fb27fc0
Arturs <arturs.vanags320@gmail.com>
2023-11-11 14:17:12 +0200

    Update package.json and logo.tsx

--- src/config/logo.tsx
+++ src/config/logo.tsx
@@ -1,23 +1,16 @@
 'use client';
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+import { useMemo, useEffect, useState } from 'react';
 import Image from 'next/image';
+
 const DefaultLogo = (props: React.ComponentProps<'img'>) => {
   const { resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-  let logoSrc;
-  switch (resolvedTheme) {
-    case 'dark':
-      logoSrc = '/logo-white.svg';
-      break;
-    case 'light':
-      logoSrc = '/logo-black.svg';
-      break;
-    default:
-      logoSrc = '/logo-white.svg';
-      break;
-  }
-  //useEffect only runs on the client, so now we can safely show the UI
+
+  const logoSrc = useMemo(() => {
+    return resolvedTheme === 'light' ? '/logo-black.svg' : '/logo-white.svg';
+  }, [resolvedTheme]);
+
   useEffect(() => {
     setMounted(true);
   }, []);
@@ -27,15 +20,15 @@ const DefaultLogo = (props: React.ComponentProps<'img'>) => {
   }
 
   return (
-    <>
+    <div className={props.className}>
       <Image
         src={logoSrc}
         alt="Logo"
         width={100}
         height={100}
-        className={props.className}
+        priority={true}
       />
-    </>
+    </div>
   );
 };
 
