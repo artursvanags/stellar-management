7ea98bfdf2385bd0994ac8270efe1eeda23a0014
Arturs <arturs.vanags320@gmail.com>
2024-01-22 16:55:26 +0200

    Refactor FilamentTemplate component

--- src/components/dashboard/filaments/templates/filament-component.tsx
+++ src/components/dashboard/filaments/templates/filament-component.tsx
@@ -1,16 +1,13 @@
 import { columns } from '@/components/dashboard/filaments/components/table/columns';
 import { DataTable } from '@/components/dashboard/filaments/components/table/dataTable';
 
-import { AddFilamentButton } from '@/components/modals/filamentModal';
 import { getData } from '@/lib/utils/get-data';
 
 export default async function FilamentTemplate() {
   const { filaments } = await getData();
+
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex">
-        <AddFilamentButton className="ml-auto" />
-      </div>
+    <div className="relative flex flex-col gap-2">
       <div>
         <DataTable columns={columns} data={filaments} />
       </div>
