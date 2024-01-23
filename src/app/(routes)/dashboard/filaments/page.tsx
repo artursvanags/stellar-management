566a788b88d375fe1028990501ee9472f14a2e40
Arturs <arturs.vanags320@gmail.com>
2024-01-23 14:42:49 +0200

    Refactor FilamentsPage component

--- src/app/(routes)/dashboard/filaments/page.tsx
+++ src/app/(routes)/dashboard/filaments/page.tsx
@@ -1,11 +1,8 @@
 import { AddFilamentButton } from '@/components/dashboard/components/add-filament-button';
 import FilamentTemplate from '@/components/dashboard/filaments/templates/filament-component';
 import SectionHeader from '@/components/global/section-header';
-import { getData } from '@/lib/utils/get-data';
 
-const FilamentsPage = async () => {
-  const { user } = await getData();
-  if (!user) return null;
+const FilamentsPage = () => {
   return (
     <div className="space-y-6">
       <SectionHeader
