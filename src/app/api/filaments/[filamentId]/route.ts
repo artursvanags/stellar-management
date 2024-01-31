fe23844b381bdf97cb0b4a9d80188c79440fec13
Arturs <arturs.vanags320@gmail.com>
2024-02-01 00:03:45 +0200

    Updated APIs with data mutation actions.

--- src/app/api/filaments/[filamentId]/route.ts
+++ src/app/api/filaments/[filamentId]/route.ts
@@ -1,100 +1,35 @@
-import { checkUserSession } from '@/lib/utils/checkUserSession';
-
 import { NextResponse } from 'next/server';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth/options';
 
-import prismadb from '@/lib/utils/database';
-import { getUpdateObject } from '@/lib/utils';
-
-
-export async function GET(
-  request: Request,
-  { params }: { params: { filamentId: string } },
-) {
-  try {
-    const { user, error } = await checkUserSession();  
-    if (error) {
-      return new NextResponse(error.message, { status: error.status });
-    }
-    
-    const data = await prismadb.filament.findUnique({
-      where: { 
-        userId: user?.id,
-        id: params.filamentId },
-      include: {
-        manufacturer: true,
-        material: true,
-        color: true,
-        tags: true,
-      },
-    });
-
-    if (!data) {
-      return new NextResponse('Data not found', { status: 404 });
-    }
-    
-    const formatData = {
-      id: data.id,
-      userId: data.userId,
-      status: data.status,
-      diameter: data.diameter,
-      manufacturer: data.manufacturer.name,
-      material: data.material.name,
-      color: data.color.name,
-      weight: data.weight,
-      remainingWeight: data.remainingWeight,
-      createdAt: data.createdAt,
-      updatedAt: data.updatedAt,
-      tags: data.tags, 
-    };
-    
-    if (!data) {
-      return new NextResponse('Data not found', { status: 404 });
-    }
-
-    return new NextResponse(JSON.stringify(formatData, null, 2), {
-      status: 200,
-    });
-  } catch (err) {
-    console.error(err);
-    return new NextResponse('Internal Error', { status: 500 });
-  }
-}
+import { getFilament, getUser } from '@/lib/actions/get-data-actions';
+import {
+  deleteFilament,
+  updateFilament,
+} from '@/lib/actions/mutate-data-actions';
 
 export async function PATCH(
   request: Request,
   { params }: { params: { filamentId: string } },
 ) {
   try {
-    const { user, error } = await checkUserSession();  
-    if (error) {
-      return new NextResponse(error.message, { status: error.status });
+    const user = await getUser();
+    if (!user) {
+      return new NextResponse('Unauthorized', { status: 404 });
+    }
+    const body = await request.json();
+
+    const filament = await getFilament(params.filamentId);
+    if (!filament) {
+      return new NextResponse('Filament not found', { status: 404 });
     }
 
-    const body = await request.json();
-    console.log(body)
-    await prismadb.filament.update({
-      where: {
-        id: params.filamentId,
-      },
-      data: {
-        ...body,
-        manufacturer: getUpdateObject(body.manufacturer),
-        material: getUpdateObject(body.material),
-        color: getUpdateObject(body.color),
-        tags: {
-          connectOrCreate: body.tags.map((tag: any) => ({
-            where: { id: tag.id },
-            create: { id: tag.id, name: tag.name },
-          })),
-        },
-      },
-    });
+    await updateFilament(params.filamentId, body);
 
-    return new NextResponse('Update successful!', { status: 200 });
+    return new NextResponse('Filament has been updated successfully!', {
+      status: 200,
+    });
   } catch (error) {
-    return new NextResponse(`Internal error - ${error}`, { status: 500 });
+    console.log('[PATCH-ERROR]', error);
+    return new NextResponse(`Internal error`, { status: 500 });
   }
 }
 
@@ -103,80 +38,21 @@ export async function DELETE(
   { params }: { params: { filamentId: string } },
 ) {
   try {
-    const session = await getServerSession(authOptions);
-    if (!session) {
-      return NextResponse.json({ error: 'Session not found' }, { status: 401 });
-    }
-
-    const user = await prismadb.user.findUnique({
-      where: { email: session?.user?.email! },
-    });
+    const user = await getUser();
     if (!user) {
-      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+      return new NextResponse('Unauthorized', { status: 404 });
     }
 
-    const filament = await prismadb.filament.findUnique({
-      where: {
-        id: params.filamentId,
-      },
-      include: {
-        manufacturer: true,
-        color: true,
-        material: true,
-        tags: true,
-      },
-    });
-
+    const filament = await getFilament(params.filamentId);
     if (!filament) {
-      throw new Error('Filament not found');
+      return new NextResponse('Filament not found', { status: 404 });
     }
 
-    await prismadb.filament.delete({
-      where: {
-        id: params.filamentId,
-      },
-    });
-
-    const { manufacturer, color, material, tags } = filament;
-
-    if (
-      manufacturer &&
-      (await prismadb.filament.count({
-        where: { manufacturerId: manufacturer.id },
-      })) === 0
-    ) {
-      await prismadb.manufacturer.delete({ where: { id: manufacturer.id } });
-    }
-
-    if (
-      color &&
-      (await prismadb.filament.count({ where: { colorId: color.id } })) === 0
-    ) {
-      await prismadb.color.delete({ where: { id: color.id } });
-    }
-
-    if (
-      material &&
-      (await prismadb.filament.count({
-        where: { materialId: material.id },
-      })) === 0
-    ) {
-      await prismadb.material.delete({ where: { id: material.id } });
-    }
-
-    for (const tag of tags) {
-      if (
-        (await prismadb.filament.count({
-          where: { tags: { some: { id: tag.id } } },
-        })) === 0
-      ) {
-        await prismadb.tag.delete({ where: { id: tag.id } });
-      }
-    }
+    await deleteFilament(params.filamentId);
 
     return new NextResponse('Delete successful!', { status: 200 });
   } catch (error) {
-    console.log(error);
+    console.log('[DELETE-ERROR]', error);
     return new NextResponse('Internal error', { status: 500 });
   }
 }
--- src/app/api/filaments/route.ts
+++ src/app/api/filaments/route.ts
@@ -1,154 +1,58 @@
 import { NextResponse } from 'next/server';
-import { checkUserSession } from '@/lib/utils/checkUserSession';
-
-import prismadb from '@/lib/utils/database';
-
-export const dynamic = 'force-dynamic';
-
-export async function GET() {
-  try {
-    const { user, error } = await checkUserSession();  
-    if (error) {
-      return new NextResponse(error.message, { status: error.status });
-    }
-
-    const data = await prismadb.filament.findMany({
-      where: { userId: user?.id },
-      include: {
-        tags: true,
-      },
-    });
-
-    // Restructure the data
-    const formatData = data.map((item) => ({
-...item,
-      tags: item.tags.map((tag) => tag.name),
-    }));
-
-    return new NextResponse(JSON.stringify(formatData, null, 2), {
-      status: 200,
-    });
-  } catch (err) {
-    console.error(err);
-    return new NextResponse('Internal Error', { status: 500 });
-  }
-}
+import { getUser } from '@/lib/actions/get-data-actions';
+import {
+  createMultipleFilaments,
+  deleteMultipleFilaments,
+  updateMultipleFilaments,
+} from '@/lib/actions/mutate-data-actions';
 
 export async function POST(request: Request) {
   try {
-    const { user, error } = await checkUserSession();  
-    if (error) {
-      return new NextResponse(error.message, { status: error.status });
+    const user = await getUser();
+    if (!user) {
+      return new NextResponse('Unauthorized', { status: 404 });
     }
-
     const body = await request.json();
+    await createMultipleFilaments(user.id, body);
 
-    const operations = body.map((item: any) => {
-      const filamentData = {
-        ...item,
-        userId: user?.id,
-      };
-    
-      if (item.tags && item.tags.length > 0) {
-        filamentData.tags = {
-          connectOrCreate: item.tags.map((tag: any) => ({
-            where: { name: tag.name, userId: user?.id },
-            create: { name: tag.name, userId: user?.id },
-          })),
-        };
-      }
-    
-      return prismadb.filament.create({
-        data: filamentData,
-      });
-    });
-    
-    await prismadb.$transaction(operations);
     return new NextResponse('Post successful!', { status: 200 });
   } catch (error) {
-    console.log(error);
-    return new NextResponse('Internal Error', { status: 500 });
+    console.log('[POST-ERROR]', error);
+    return new NextResponse(`Internal error`, { status: 500 });
   }
 }
 
 export async function PATCH(request: Request) {
   try {
-    const { user, error } = await checkUserSession();  
-    if (error) {
-      return new NextResponse(error.message, { status: error.status });
+    const user = await getUser();
+    if (!user) {
+      return new NextResponse('Unauthorized', { status: 404 });
     }
 
     const body = await request.json();
-    const updates = body.map(
-      ({ id, ...data }: { id: string; [key: string]: any }) => ({ id, data }),
-    );
 
-    for (const update of updates) {
-      await prismadb.filament.update({
-        where: { 
-          userId: user?.id,
-          id: update.id },
-        data: update.data,
-      });
-    }
+    await updateMultipleFilaments(body);
 
     return new NextResponse('Update successful!', { status: 200 });
   } catch (error) {
-    return new NextResponse(`Internal error - ${error}`, { status: 500 });
+    console.log('[PATCH-ERROR]', error);
+    return new NextResponse(`Internal error`, { status: 500 });
   }
 }
 
 export async function DELETE(request: Request) {
   try {
-    const { user, error } = await checkUserSession();  
-    if (error) {
-      return new NextResponse(error.message, { status: error.status });
+    const user = await getUser();
+    if (!user) {
+      return new NextResponse('Unauthorized', { status: 404 });
     }
 
     const body = await request.json();
-
-    // Get the filaments to be deleted
-    const filaments = await prismadb.filament.findMany({
-      where: {
-        userId: user?.id,
-        id: {
-          in: body,
-        },
-      },
-      include: {
-        tags: true,
-      },
-    });
-
-    // Delete the filaments
-    await prismadb.filament.deleteMany({
-      where: {
-        id: {
-          in: body,
-        },
-      },
-    });
-
-    // Delete tags that are no longer present in any other filaments
-    const tagsToDelete = filaments.flatMap((filament) => filament.tags).map(tag => tag.id.toString());
-    await prismadb.tags.deleteMany({
-      where: {
-        id: {
-          in: tagsToDelete,
-        },
-        filaments: {
-          none: {
-            id: {
-              in: body,
-            },
-          },
-        },
-      },
-    });
+    await deleteMultipleFilaments(body);
 
     return new NextResponse('Delete successful!', { status: 200 });
   } catch (error) {
-    console.log(error);
-    return new NextResponse(`Internal error ${error}`, { status: 500 });
+    console.log('[DELETE-ERROR]', error);
+    return new NextResponse(`Internal error`, { status: 500 });
   }
 }
