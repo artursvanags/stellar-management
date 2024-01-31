014694c8bf934dbdc464a8e917c6248ef71b4e7f
Arturs <arturs.vanags320@gmail.com>
2024-02-01 00:04:36 +0200

    Update getFilaments to getMultipleFilaments in FilamentTemplate

--- src/components/dashboard/filaments/templates/filament-template.tsx
+++ src/components/dashboard/filaments/templates/filament-template.tsx
@@ -1,16 +1,15 @@
 import { columns } from '@/components/dashboard/filaments/components/table/columns';
 import { DataTable } from '@/components/dashboard/filaments/components/table/dataTable';
 
-import { getFilaments} from '@/lib/actions/get-data';
-
+import { getMultipleFilaments } from '@/lib/actions/get-data-actions';
 
 export default async function FilamentTemplate() {
-  const filaments = await getFilaments();
+  const filaments = await getMultipleFilaments();
   return (
-      <div className="relative flex flex-col gap-2">
-        <div>
-          <DataTable columns={columns} data={filaments} />
-        </div>
+    <div className="relative flex flex-col gap-2">
+      <div>
+        <DataTable columns={columns} data={filaments} />
       </div>
+    </div>
   );
 }
