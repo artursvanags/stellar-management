954e19881a6ed6f14d89bfe7eb13677c6285637a
Arturs <arturs.vanags320@gmail.com>
2024-02-03 17:38:20 +0200

    Refactor status column in Filaments table

--- src/components/dashboard/filaments/components/table/status-column.tsx
+++ src/components/dashboard/filaments/components/table/status-column.tsx
@@ -7,24 +7,52 @@ import { Filaments } from '@/types/database';
 
 interface StatusColumnProps {
   status: Filaments['status'];
-
 }
 
+const statusBadge = {
+  new: {
+    label: 'New',
+    color:
+      'bg-emerald-400 text-emerald-900 dark:bg-emerald-950 dark:text-emerald-500',
+  },
+  used: {
+    label: 'Used',
+    color:
+      'bg-yellow-400 text-yellow-900 dark:bg-yellow-950 dark:text-yellow-500',
+  },
+  archived: {
+    label: 'Archived',
+    color:
+      'bg-neutral-200 text-neutral-500 dark:bg-neutral-900 dark:text-neutral-500',
+  },
+  in_use: {
+    label: 'In Use',
+    color:
+      'bg-orange-400 text-orange-900 dark:bg-orange-950 dark:text-orange-500',
+  },
+};
+
 export function StatusColumn({ status }: StatusColumnProps) {
-  let color = '';
-  if (status === 'new') {
-    color = 'dark:bg-emerald-950 bg-emerald-400 text-emerald-900 dark:text-emerald-500';
-  } else if (status === 'used') {
-    color = 'dark:bg-orange-950 bg-orange-400 text-orange-900 dark:text-orange-500';
-  } else if (status === 'archived') {
-    color = 'dark:bg-neutral-900 bg-neutral-200 text-neutral-500 dark:text-neutral-500';
-  } else {
-    color = 'text-muted-foreground';
-  }
+  const { label, color } = statusBadge[status as keyof typeof statusBadge];
+  const isStatusInUse = status === 'in_use';
+
   return (
-    <Badge variant={'outline'} className={`border-transparent text-xs font-medium ${color}`}>
-      <Icons.Dot strokeWidth={4} className="h-4 w-4" />
-      {status.slice(0, 1).toUpperCase() + status.slice(1)}
+    <Badge
+      variant='outline'
+      className={`border-transparent text-xs font-medium ${color}`}
+    >
+      {isStatusInUse ? (
+        <div className="relative h-4 w-4">
+          <Icons.Dot
+            strokeWidth={4}
+            className="absolute h-4 w-4 animate-ping"
+          />
+          <Icons.Dot strokeWidth={4} className="h-4 w-4" />
+        </div>
+      ) : (
+        <Icons.Dot strokeWidth={4} className="h-4 w-4" />
+      )}
+      {label}
     </Badge>
   );
 }
